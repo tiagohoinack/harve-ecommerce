ef0a0ee48f76b072fbbeab30157d881c168e8b5b
bruno_pedroso17@hotmail.com <dev10@lwtecnologia.com.br>
2025-01-27 19:40:01 -0300

    revisao componente: lisat produtos

---
+++ src/_aulas/turma 15/aula-3/componentes/ListaProdutos.jsx
@@ -0,0 +1,15 @@
+import { Col, Row } from "react-bootstrap";
+
+function ListaProdutos() {
+  return (
+    <>
+      <Row className="justify-content-md-center">
+        <Col md={4}>
+          <h1>Lista de Produtos</h1>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export { ListaProdutos };
