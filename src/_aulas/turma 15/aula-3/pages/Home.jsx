efdcff1cbc59749c8b0bb80f503da5aa3a4e0d40
bruno_pedroso17@hotmail.com <dev10@lwtecnologia.com.br>
2025-01-27 20:21:45 -0300

    revisao componente: home e produtos

---
+++ src/_aulas/turma 15/aula-3/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div className="App">
+      HOME EXEMPLO
+    </div>
+  );
+}
+
+export { Home };
---
+++ src/_aulas/turma 15/aula-3/pages/Produto.jsx
@@ -0,0 +1,10 @@
+
+function Produtos() {
+  return (
+    <div className="App">
+      Pagina Produtos EXEMPLO
+    </div>
+  );
+}
+
+export { Produtos };
