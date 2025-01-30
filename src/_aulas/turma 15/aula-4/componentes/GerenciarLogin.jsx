db23033016dec3f23c1f06b3ed3a7fa9fcef6569
bruno_pedroso17@hotmail.com <dev10@lwtecnologia.com.br>
2025-01-29 22:12:24 -0300

    aula -4: implematção

--- src/_aulas/turma 15/aula-4/componentes/GerenciarLogin.jsx
+++ src/_aulas/turma 15/aula-4/componentes/GerenciarLogin.jsx
@@ -1,7 +1,51 @@
-import { Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
 
 function GerenciarLogin() {
-  return <Row>LOGIN</Row>;
+  const [token, setToken] = useState();
+
+  function pegarLocalStorage() {
+    const token = localStorage.getItem("token");
+    const nomeUsuario = localStorage.getItem("nomeUsuario");
+    const emailUsuario = localStorage.getItem("emailUsuario");
+
+    setToken(token);
+  }
+
+  function removerDadosLocalStorage() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("nomeUsuario");
+    localStorage.removeItem("emailUsario");
+    pegarLocalStorage();
+  }
+
+  useEffect(() => {
+    pegarLocalStorage();
+  }, []);
+
+  if (!token) {
+    return (
+      <Row>
+        <Col md={12}>
+          <a href="/login">
+            <Button variant="primary">Login</Button>{" "}
+          </a>
+        </Col>
+      </Row>
+    );
+  }
+
+  return (
+    <Row>
+      <Col md={7}>
+        <i className="fa fa-user"> nome:</i>
+        <i className="fa fa-at"> email:</i>
+      </Col>
+      <Col md={12}>
+        <Button variant="danger">Sair</Button>{" "}
+      </Col>
+    </Row>
+  );
 }
 
 export { GerenciarLogin };
