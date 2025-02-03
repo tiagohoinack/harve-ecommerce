81891f3ad42aed760eb7741112af5edd44c9c413
bruno_pedroso17@hotmail.com <dev10@lwtecnologia.com.br>
2025-02-03 20:10:52 -0300

    aula -5 final: implementanção add produtos

--- src/_aulas/turma 15/aula-5-final/App.js
+++ src/_aulas/turma 15/aula-5-final/App.js
@@ -4,7 +4,7 @@ import "font-awesome/css/font-awesome.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { RouteApp } from "./route";
-import { CarrinhoContextProvider } from "./hooks/CarrinhoContext";
+import CarrinhoContextProvider from "./hooks/CarrinhoContext";
 
 function App() {
   return (
--- src/_aulas/turma 15/aula-5-final/route/index.jsx
+++ src/_aulas/turma 15/aula-5-final/route/index.jsx
@@ -1,13 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { IBGE } from "../_aulas/anteriores/aula3/IBGE";
-import { IBGEv2 } from "../_aulas/anteriores/aula3/IBGEv2";
-import { LoginV2 } from "../_aulas/aula5/LoginV2";
 import { Carrinho } from "../pages/Carrinho";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Produto } from "../pages/Produto";
-import { BaseSistema } from "./v2/BaseSistema";
-import { MinhaRotas } from "./v2/MinhaRotas";
 export function RouteApp() {
   return (
     <BrowserRouter>
