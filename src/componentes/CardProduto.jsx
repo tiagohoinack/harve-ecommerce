0e0f944e83920c7feacdd9be12bd79eb11094caa
Tiago Hoinack <tiagohoinack@hotmail.com>
2025-08-26 21:01:44 -0300

    Implementação da função de listar todos os produtos na página produtos

--- src/componentes/CardProduto.jsx
+++ src/componentes/CardProduto.jsx
@@ -1,17 +1,15 @@
-import { Button, Card, Col } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 function CardProduto(nome, preco, urlImagem) {
   return (
-    <Col>
-      <Card style={{ width: "18rem" }}>
+      <Card className="h-100" style={{ width: "18rem" }}>
         <Card.Img variant="top" src={urlImagem} />
         <Card.Body>
           <Card.Title>{nome}</Card.Title>
           <Card.Text>Preço: R${preco}</Card.Text>
-          <Button variant="primary">Ver Produto</Button>
+          <Button variant="primary">Adicionar ao Carrinho</Button>
         </Card.Body>
       </Card>
-    </Col>
   );
 }
 
--- src/componentes/ListarProdutos.jsx
+++ src/componentes/ListarProdutos.jsx
@@ -23,15 +23,13 @@ function ListarProdutos() {
     <div>
       <h1>Lista de Produtos</h1>
       <Container>
-        {produtos.map((produto) => {
-          CardProduto(produto.title, produto.price, produto.image)
-        })}
-          {/* <div>
-           <img src={produto.image} alt={produto.title} width={200} />
-           <p>Nome: {produto.title}</p>
-           <p>Preço: {produto.price}</p>
-           <hr />
-        </div> */}
+        <Row>
+          {produtos.map((produto, idx) => (
+            <Col key={idx} className="mb-3 h-100">
+              {CardProduto(produto.title, produto.price, produto.image)}
+            </Col>
+          ))}
+        </Row>
       </Container>
     </div>
   );
